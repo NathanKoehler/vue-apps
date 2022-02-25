bdaf4424e712a64a7f85e65b56b1662e6af6b05d
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2022-02-25 12:42:40 -0500

    Changed TextText asset

--- src/apps/Room_ARVR/Alyx/App.vue
+++ src/apps/Room_ARVR/Alyx/App.vue
@@ -2,11 +2,10 @@
   <div> 
   <div id="room" class="darkwall">
   <div class="spacer">
-  <img src="../../../assets/images/Room5/Alyx-splash.png" width="400" >
+  <img src="../../../assets/images/Room5/Alyx-splash.png" width="500" >
   <br><br>
-  <Title msg="HalfLife: Alyx" />
-	<div class="squareoff"> First person shooter games such as  <a href="https://www.natekoe.com/" target="_blank">HalfLife: Alyx </a> have long used 3-D graphics to create an immersive experience for millions of players. And for decades, 
-    players on computers and game consoles have yearned for true VR so that they could fall through the screen into the worlds on the other side.</div> 
+  <Title msg="Imagine A World" />
+	<div class="squareoff"> Imagine a world you are living in, full of mystery and wonder. Now imagine you were flying.</div> 
   </div>
   </div>
   </div> 
