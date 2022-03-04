a5e25973de6aa0b31d371ec7918aa6077eb42e3f
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2022-03-04 12:08:39 -0500

    Changed the Alyx component (renamed it and modified values)

---
+++ src/apps/Room_ARVR/WhatIsARVR1_History/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div> 
+  <div id="room" class="darkwall">
+  <div class="spacer">
+  <img src="../../../assets/images/Room5/Alyx-splash.png" width="500" >
+  <br><br>
+  <Title msg="History of AR/VR" />
+	<div class="squareoff">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus pretium quam vulputate dignissim. Sed enim ut sem viverra aliquet. Lobortis feugiat vivamus at augue eget. Metus vulputate eu scelerisque felis imperdiet. In fermentum et sollicitudin ac orci phasellus.</div> 
+  </div>
+  </div>
+  </div> 
+</template>
+
+<script setup>
+import Title from '../../../components/CenterTitle.vue'
+
+import "../../../assets/top.css"
+import "../../../assets/room.css"
+</script>
+
+<style scoped>
+
+</style>
