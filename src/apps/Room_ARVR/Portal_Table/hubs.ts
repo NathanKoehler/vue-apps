a9e4883dfa7e2effb3fd87bb7ebb65405f84c1ca
Nathan Koehler <nkoehler3@gatech.edu>
2022-03-18 02:17:57 -0400

    changed portal table sizing

--- src/apps/Room_ARVR/Portal_Table/hubs.ts
+++ src/apps/Room_ARVR/Portal_Table/hubs.ts
@@ -9,7 +9,7 @@ class HubsApp extends HubsAppProto {
 }
 
 var init = function (params: any = {}) {
-    let app = new HubsApp(900, 475, params)
+    let app = new HubsApp(600, 475, params)
     app.mount()
     return app
 }
