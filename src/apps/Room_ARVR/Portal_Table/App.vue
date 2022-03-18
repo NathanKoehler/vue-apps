6e18861c1f909094aa495ae34441b84ed942b485
Nathan Koehler <nkoehler3@gatech.edu>
2022-03-18 02:54:12 -0400

    changed room content

--- src/apps/Room_ARVR/Portal_Table/App.vue
+++ src/apps/Room_ARVR/Portal_Table/App.vue
@@ -6,7 +6,9 @@
   <div style="margin-left: 30px">
   <Title msg="Mediums and Portals" />
   <br><br>
-  <div class="squareoff">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus pretium quam vulputate dignissim. Sed enim ut sem viverra aliquet. Lobortis feugiat vivamus at augue eget. Metus vulputate eu scelerisque felis imperdiet. In fermentum et sollicitudin ac orci phasellus.</div> 
+  <div class="squareoff">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus pretium quam vulputate dignissim. Sed enim ut sem viverra aliquet. Lobortis feugiat vivamus at augue eget. Metus vulputate eu scelerisque felis imperdiet. In fermentum et sollicitudin ac orci phasellus.</div>
+  <br>
+  <div class="squareoff">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus pretium quam vulputate dignissim. Sed enim ut sem viverra aliquet. Lobortis feugiat vivamus at augue eget. Metus vulputate eu scelerisque felis imperdiet. In fermentum et sollicitudin ac orci phasellus.</div>  
   </div>
   </div>
   </div>
