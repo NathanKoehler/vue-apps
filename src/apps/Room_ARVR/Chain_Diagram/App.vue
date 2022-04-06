776484a8157de3cb248b132f5b83289e289f34b0
Nathan Koehler <40174078+NathanKoehler@users.noreply.github.com>
2022-04-06 05:35:24 -0400

    trying to get the pictures to load

--- src/apps/Room_ARVR/Chain_Diagram/App.vue
+++ src/apps/Room_ARVR/Chain_Diagram/App.vue
@@ -3,7 +3,7 @@
   <div id="room" class="darkwall center-mount">
   <div class="sidebyside">
   <img src="../../../assets/images/Room5/Alyx-splash.png" width="400" >
-  <img src="../../../assets/images/Room_ARVR/chain_diagram.png" width="400">
+  <img src="../../../assets/images/Room_ARVR/chain-diagram.png" width="400">
   <div style="margin-left: 40px">
   <Title msg="Augmented to Reality" />
   <br><br>
